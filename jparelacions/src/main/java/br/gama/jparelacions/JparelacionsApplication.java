fa4756246e30d812e4464b860b2464a48ee719aa
EmersonPaduan <emerson.paduan@gmail.com>
2023-04-19 10:05:43 -0300

    JPA initial commit

---
+++ jparelacions/src/main/java/br/gama/jparelacions/JparelacionsApplication.java
@@ -0,0 +1,13 @@
+package br.gama.jparelacions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparelacionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparelacionsApplication.class, args);
+	}
+
+}
