e0796f8dba665eb49c6b59d2ea00324c7fa4e8c3
EmersonPaduan <emerson.paduan@gmail.com>
2023-07-12 09:12:02 -0300

    exercicio stream

---
+++ exercicios/exercicio-stream/Main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+// Listar todas as pessoas
+// Listar as 10 primeiras pessoas
+// Listar as pessoas que são do Brasil
+// Listar as pessoas do Brasil com o nome em Uppercase e suas idades
+// Listar as pessoas que são do Brasil, em ordem de nome
+// Listar as pessoas que são do Brasil, em ordem de idade
+// Contar quantas pessoas tem mais de 45 anos
+// Calcular a média de idade das pessoas
+
+public class Main {
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = Dados.getPessoas();
+
+    }
+
+}
