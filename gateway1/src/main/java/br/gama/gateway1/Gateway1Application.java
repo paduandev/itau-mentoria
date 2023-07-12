73eeb50cbb02b6bb4fda279be4de4e4d643264d2
EmersonPaduan <emerson.paduan@gmail.com>
2023-07-12 11:01:36 -0300

    gateway-v01

---
+++ gateway1/src/main/java/br/gama/gateway1/Gateway1Application.java
@@ -0,0 +1,13 @@
+package br.gama.gateway1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gateway1Application.class, args);
+	}
+
+}
---
+++ gateway1/src/test/java/br/gama/gateway1/Gateway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gama.gateway1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gateway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
